372c6289a4fef73f227227237241c3d9328bdca6
Anas Waseem <anas@attribes.com>
2017-07-21 11:20:58 +0500

    show wait time in hours & minutes on reports

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,15 @@
 module ReportsHelper
+  def formatted_duration(total_minute)
+    duration = ""
+    hours = total_minute / 60
+    minutes = (total_minute) % 60
+    if hours > 0
+      duration = "#{hours} #{'hr'.pluralize(hours)}"
+      duration += " and #{minutes} #{'minute'.pluralize(minutes)}" if minutes > 0
+    else
+      duration = "#{minutes} #{'minute'.pluralize(minutes)}"
+    end
+    duration
+  end
+
 end
