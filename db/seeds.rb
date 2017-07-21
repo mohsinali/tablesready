cf24a5766c73c14d6df6babcb99e9d7f17de2fd2
Anas Waseem <anas@attribes.com>
2017-07-21 13:11:12 +0500

    db/seed fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
 
 CreatePlanService.new.call
 puts "Plans created!"
