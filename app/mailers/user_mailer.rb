32c7824abb93b5489d48b3cede31f21c16a18ad9
Anas Waseem <anas@attribes.com>
2017-08-21 12:52:21 +0500

    email configuration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   default from: "Admin <admin@ready-text.com>"
   # layout 'mailer'
 
-  def welcome_email(user)
+  def test_email(user)
     @user = user
     mail(to: @user.email, subject: @subject)
   end
