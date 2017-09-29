1fc9c0d23740541b9b6150fa793556bdf1e7a32a
Sarwan Kumar <srladuani@gmail.com>
2017-09-29 16:51:42 +0500

    clickatell message feature changes

--- lib/clickatell_box.rb
+++ lib/clickatell_box.rb
@@ -3,7 +3,8 @@ require 'rest_client'
 class ClickatellBox
   def initialize type="one-way"
     @type = type
-    @authorization = @type == 'one-way' ? ENV['CLICKATELL_ONE_WAY_KEY'] : ENV['CLICKATELL_TWO_WAY_KEY']
+    #@authorization = @type == 'one-way' ? ENV['CLICKATELL_ONE_WAY_KEY'] : ENV['CLICKATELL_TWO_WAY_KEY']
+    @authorization = ENV['CLICKATELL_TWO_WAY_KEY']
     @headers = {"Content-Type"=> "application/json","Accept"=> "application/json","Authorization"=> @authorization}
     @url = "https://platform.clickatell.com/messages"
   end
@@ -16,11 +17,12 @@ class ClickatellBox
   ######################################
   def send_sms recipents,content
     begin
-      if @type == 'one-way'
-        payload = {content: content ,to: recipents}.to_json
-      else
-        payload = {content: content ,to: recipents,from: ENV['CLICKATELL_FROM_NUMBER']}.to_json
-      end
+      # if @type == 'one-way'
+      #   payload = {content: content ,to: recipents}.to_json
+      # else
+      #   payload = {content: content ,to: recipents,from: ENV['CLICKATELL_FROM_NUMBER']}.to_json
+      # end
+      payload = {content: content ,to: recipents,from: ENV['CLICKATELL_FROM_NUMBER']}.to_json
       response = RestClient.post(@url, payload, @headers)
       response = {error: false,data: response}
     rescue Exception => e
